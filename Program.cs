6755700bbcce50e5566250354d9ac7c1f4a2f146
hack2root <me@experimentalcommunity.org>
2019-02-25 23:57:48 +0300

    initial commit

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.IO;
+
+namespace env_core3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var builder = new ConfigurationBuilder()
+                .AddEnvironmentVariables();
+            var value = Environment.GetEnvironmentVariable("TestKey");
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ git-create.ps1
@@ -0,0 +1,7 @@
+& echo "# env-core3" > README.md
+& echo "https://stackoverflow.com/questions/54874088/reading-content-of-environment-variables-dialog/54874383#54874383" >> README.md
+& git init
+& git add .
+& git commit -m "initial commit"
+& git remote add origin https://github.com/hack2root/env-core3.git
+& git push -u origin master
